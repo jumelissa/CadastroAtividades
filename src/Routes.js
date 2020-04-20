9acdf39c8e1537a85598b88f8769d821485b97aa
Santos, Juliana M <juliana.m.santos@accenture.com>
2020-04-20 15:58:18 -0300

    all files app

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './Components/Login/index';
+import Cadastro from './Components/Cadastro/index';
+import Detalhes from './Components/Detalhes/index';
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/Cadastro" component={Cadastro} />
+            <Route path="/Detalhes" component={Detalhes} />
+            
+        </Switch>
+    </BrowserRouter>
+    )
+}
+
+export default Router;
